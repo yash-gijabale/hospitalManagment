ffba81be78b3a2bb64a413e9f75e71df25f47505
Yash Gijabale <yashgijabale@gmail.com>
2024-04-01 23:07:19 +0530

    pateint paroperty modal

--- frontend/src/pages/patient/PatientDetails.jsx
+++ frontend/src/pages/patient/PatientDetails.jsx
@@ -15,6 +15,21 @@ import LinearProgress, {
 } from "@mui/material/LinearProgress";
 import { styled } from "@mui/material/styles";
 
+import Timeline from "@mui/lab/Timeline";
+import TimelineItem from "@mui/lab/TimelineItem";
+import TimelineSeparator from "@mui/lab/TimelineSeparator";
+import TimelineConnector from "@mui/lab/TimelineConnector";
+import TimelineContent from "@mui/lab/TimelineContent";
+import TimelineDot from "@mui/lab/TimelineDot";
+
+import Modal from "@mui/material/Modal";
+import TextField from "@mui/material/TextField";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+
+import "../common.css";
 // const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
 //   height: 10,
 //   borderRadius: 5,
@@ -28,7 +43,23 @@ import { styled } from "@mui/material/styles";
 //   },
 // }));
 
+const Modalstyle = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  borderRadius: 3,
+  boxShadow: 24,
+  p: 2,
+};
+
 const PatientDetails = () => {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
     <div className="main_view">
       <PageTitle title={"Patient Details"} />
@@ -70,12 +101,23 @@ const PatientDetails = () => {
           <Card sx={{ maxWidth: 345, marginTop: "1rem" }} disableRipple>
             <CardActionArea disableRipple>
               <CardActions>
-                <Button size="small" color="primary" variant="contained">
-                  <AddIcon />New
+                <Button
+                  size="small"
+                  color="primary"
+                  variant="contained"
+                  onClick={handleOpen}
+                >
+                  <AddIcon />
+                  New
                 </Button>
               </CardActions>
               <CardContent style={{ textAlign: "start" }}>
-                <Typography gutterBottom variant="span" component="div" style={{fontSize:15, marginBottom:'1rem'}}>
+                <Typography
+                  gutterBottom
+                  variant="span"
+                  component="div"
+                  style={{ fontSize: 15, marginBottom: "1rem" }}
+                >
                   <span>Blood Presure</span>
                   <Tooltip title={50}>
                     <LinearProgress
@@ -87,7 +129,12 @@ const PatientDetails = () => {
                   </Tooltip>
                 </Typography>
 
-                <Typography gutterBottom variant="span" component="div" style={{fontSize:15,marginBottom:'1rem'}}>
+                <Typography
+                  gutterBottom
+                  variant="span"
+                  component="div"
+                  style={{ fontSize: 15, marginBottom: "1rem" }}
+                >
                   <span>Hemoglobian</span>
                   <Tooltip title={10}>
                     <LinearProgress
@@ -99,7 +146,12 @@ const PatientDetails = () => {
                   </Tooltip>
                 </Typography>
 
-                <Typography gutterBottom variant="span" component="div" style={{fontSize:15,marginBottom:'1rem'}}>
+                <Typography
+                  gutterBottom
+                  variant="span"
+                  component="div"
+                  style={{ fontSize: 15, marginBottom: "1rem" }}
+                >
                   <span>Hemoglobian</span>
                   <Tooltip title={70}>
                     <LinearProgress
@@ -112,53 +164,189 @@ const PatientDetails = () => {
                 </Typography>
               </CardContent>
             </CardActionArea>
+            <Modal
+              open={open}
+              onClose={handleClose}
+              aria-labelledby="modal-modal-title"
+              aria-describedby="modal-modal-description"
+            >
+              <form>
+                <Box sx={Modalstyle}>
+                  <Typography
+                    id="modal-modal-title"
+                    variant="h6"
+                    component="h2"
+                  >
+                    New Property
+                  </Typography>
+                  <Box
+                    component="form"
+                    sx={{
+                      "& > :not(style)": { m: 1, width: "44ch" },
+                    }}
+                    noValidate
+                    autoComplete="off"
+                  >
+                    <TextField
+                      id="outlined-basic"
+                      label="Property Name"
+                      variant="outlined"
+                    />
+                  </Box>
+
+                  <Box
+                    component="form"
+                    sx={{
+                      "& > :not(style)": { m: 1, width: "21ch" },
+                    }}
+                    noValidate
+                    autoComplete="off"
+                  >
+                    <TextField
+                      id="outlined-basic"
+                      label="Min Value"
+                      variant="outlined"
+                      type="number"
+                    />
+                    <TextField
+                      id="outlined-basic"
+                      label="Max Value"
+                      variant="outlined"
+                      type="number"
+                    />
+                  </Box>
+
+                  <Box
+                    component="form"
+                    sx={{
+                      "& > :not(style)": { m: 1, width: "44ch" },
+                    }}
+                  >
+                    <FormControl fullWidth>
+                      <InputLabel id="demo-simple-select-label">
+                        Color
+                      </InputLabel>
+                      <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        s
+                        label="Age"
+                      >
+                        <MenuItem value={10} color="primary">
+                          Primary
+                        </MenuItem>
+                        <MenuItem value={20} color="secondary">
+                          Secondary
+                        </MenuItem>
+                        <MenuItem value={30} color="danger">
+                          Danger
+                        </MenuItem>
+                        <MenuItem value={30} color="warning">
+                          Warning
+                        </MenuItem>
+                      </Select>
+                    </FormControl>
+                  </Box>
+                  <Button variant="contained" sx={{marginLeft:'0.5rem'}}>Add</Button>
+                </Box>
+              </form>
+            </Modal>
           </Card>
         </div>
         <div style={{ width: "70%" }}>
-        <Card disableRipple>
-            <CardActionArea disableRipple>
-              <CardActions>
-                <Button size="small" color="primary" variant="contained">
-                  <AddIcon />New
-                </Button>
-              </CardActions>
-              <CardContent style={{ textAlign: "start" }}>
-                <Typography gutterBottom variant="span" component="div" style={{fontSize:15, marginBottom:'1rem'}}>
-                  <span>Blood Presure</span>
-                  <Tooltip title={50}>
-                    <LinearProgress
-                      color="secondary"
-                      style={{ height: "10px", borderRadius: 5 }}
-                      variant="determinate"
-                      value={50}
-                    />
-                  </Tooltip>
-                </Typography>
-
-                <Typography gutterBottom variant="span" component="div" style={{fontSize:15,marginBottom:'1rem'}}>
-                  <span>Hemoglobian</span>
-                  <Tooltip title={10}>
-                    <LinearProgress
-                      color="warning"
-                      style={{ height: "10px", borderRadius: 5 }}
-                      variant="determinate"
-                      value={10}
-                    />
-                  </Tooltip>
-                </Typography>
-
-                <Typography gutterBottom variant="span" component="div" style={{fontSize:15,marginBottom:'1rem'}}>
-                  <span>Hemoglobian</span>
-                  <Tooltip title={70}>
-                    <LinearProgress
-                      color="error"
-                      style={{ height: "10px", borderRadius: 5 }}
-                      variant="determinate"
-                      value={70}
-                    />
-                  </Tooltip>
-                </Typography>
-              </CardContent>
+          <Card
+            disableRipple
+            style={{
+              backgroundColor: "#FFF",
+              "&:hover": {
+                //you want this to be the same as the backgroundColor above
+                backgroundColor: "#FFF",
+              },
+            }}
+            className="timeline_card"
+          >
+            <CardActionArea
+              disableRipple
+              style={{ width: "100%", justifyContent: "start" }}
+            >
+              <Typography
+                variant="h6"
+                sx={{ textAlign: "start", marginLeft: "1rem" }}
+              >
+                History
+              </Typography>
+              <Timeline sx={{ alignItems: "flex-start !important" }}>
+                <TimelineItem>
+                  <TimelineSeparator>
+                    <TimelineDot />
+                    <TimelineConnector />
+                  </TimelineSeparator>
+                  <TimelineContent>
+                    <Typography variant="h6">02-03-2024</Typography>
+                    <Typography variant="span">
+                      Spinal Osteomyelitis Surgery
+                    </Typography>
+                    <br />
+                    <Typography variant="span" color={"primary"}>
+                      Dr. S.S. Patil
+                    </Typography>
+                    <br />
+                    <Typography variant="p">Comment:</Typography>
+                    <br />
+                    <Typography variant="p">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Quasi nostrum fuga mollitia aperiam in.
+                    </Typography>
+                  </TimelineContent>
+                </TimelineItem>
+                <br />
+                <TimelineItem>
+                  <TimelineSeparator>
+                    <TimelineDot />
+                    <TimelineConnector />
+                  </TimelineSeparator>
+                  <TimelineContent>
+                    <Typography variant="h6">15-01-2024</Typography>
+                    <Typography variant="span">
+                      Spinal Osteomyelitis Surgery
+                    </Typography>
+                    <br />
+                    <Typography variant="span" color={"primary"}>
+                      Dr. S.S. Patil
+                    </Typography>
+                    <br />
+                    <Typography variant="p">Comment:</Typography>
+                    <br />
+                    <Typography variant="p">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Quasi nostrum fuga mollitia aperiam in.
+                    </Typography>
+                  </TimelineContent>
+                </TimelineItem>
+                <TimelineItem>
+                  <TimelineSeparator>
+                    <TimelineDot />
+                    <TimelineConnector />
+                  </TimelineSeparator>
+                  <TimelineContent>
+                    <Typography variant="h6">15-01-2024</Typography>
+                    <Typography variant="span">
+                      Spinal Osteomyelitis Surgery
+                    </Typography>
+                    <br />
+                    <Typography variant="span" color={"primary"}>
+                      Dr. S.S. Patil
+                    </Typography>
+                    <br />
+                    <Typography variant="p">Comment:</Typography>
+                    <br />
+                    <Typography variant="p">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Quasi nostrum fuga mollitia aperiam in.
+                    </Typography>
+                  </TimelineContent>
+                </TimelineItem>
+              </Timeline>
             </CardActionArea>
           </Card>
         </div>
